49e0268404e7e3a17c858ab33f032641b3470361
oleksii.semko <oleksii.semko@gmail.com>
2020-07-25 21:55:59 +0300

    Updates

---
+++ precache-manifest.4856a2d1873aa8d77f1d5a526954af9b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e87f6d76e56405cef9603dc77ce15302",
+    "url": "/oleksii.semko/index.html"
+  },
+  {
+    "revision": "9e3e6e409f224b8ec946",
+    "url": "/oleksii.semko/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a6f9df269ca0bc1a3a10",
+    "url": "/oleksii.semko/static/js/2.cc042a38.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/oleksii.semko/static/js/2.cc042a38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e3e6e409f224b8ec946",
+    "url": "/oleksii.semko/static/js/main.85f8401e.chunk.js"
+  },
+  {
+    "revision": "ec7ddac08a5bc9f499a6",
+    "url": "/oleksii.semko/static/js/runtime-main.285ffbc5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/oleksii.semko/static/media/logo.5d5d9eef.svg"
+  }
+]);
