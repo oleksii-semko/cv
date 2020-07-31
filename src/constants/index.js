6f8487e0e09386733968f4261f7113db61e5afc8
oleksii.semko <oleksii.semko@gmail.com>
2020-07-31 18:01:34 +0300

    SASS support added. Heading section implemented. Line separator component implemented. SV data structure created.

---
+++ src/constants/index.js
@@ -0,0 +1,43 @@
+export const PROPERTY_DESCRIPTION = 'description';
+export const PROPERTY_PERIOD = 'period';
+export const PROPERTY_DURATION = 'duration';
+export const PROPERTY_CITY = 'city';
+export const PROPERTY_COMPANY = 'company';
+export const PROPERTY_POSITION = 'position';
+export const PROPERTY_JOB_DESCRIPTION = 'jd';
+export const PROPERTY_TECHNOLOGIES = 'tech';
+export const PROPERTY_PROJECTS = 'projects';
+export const PROPERTY_EMPLOYMENT_TYPE = 'employment_type';
+
+export const PROPERTY_INSTITUTION = 'institution';
+export const PROPERTY_DEGREE = 'degree';
+
+export const PROPERTY_LINK_NAME = 'link_name';
+export const PROPERTY_LINK_URL = 'link_url';
+
+export const PROPERTY_COURSE = 'course';
+
+export const PROPERTY_LANG_NAME = 'lang_name';
+export const PROPERTY_LANG_LVL = 'lang_lvl'; // 5 lvls max
+
+export const PROPERTY_REF_FULL_NAME = 'ref_name'; 
+export const PROPERTY_REF_COMPANY = 'ref_company'; 
+export const PROPERTY_REF_PHONE = 'ref_phone'; 
+export const PROPERTY_REF_EMAIL = 'ref_email'; 
+
+export const SECTION_PROFILE = 'profile';
+export const SECTION_EMPLOYMENT = 'employment';
+export const SECTION_EDUCATION = 'education';
+export const SECTION_COURCES = 'cources';
+export const SECTION_LANGUAGES = 'languages';
+export const SECTION_REFERENCES = 'references';
+export const SECTION_LINKS = 'links';
+
+export const SECTIONS = {
+    SECTION_PROFILE,
+    SECTION_EMPLOYMENT,
+    SECTION_EDUCATION,
+    SECTION_COURCES,
+    SECTION_LANGUAGES,
+    SECTION_REFERENCES
+};
