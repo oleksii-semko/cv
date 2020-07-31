c5d2181873a6c68d2fd66b76c3b5ce6b184d675f
oleksii.semko <oleksii.semko@gmail.com>
2020-07-31 19:14:37 +0300

    Simple data provider imlemented

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Header from './components/Header'
-import LineSeparator from './components/LineSeparator'
+import Header from './components/Header';
+import LineSeparator from './components/LineSeparator';
 
 import './App.scss';
 
